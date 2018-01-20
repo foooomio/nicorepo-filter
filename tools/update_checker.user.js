67b0503b6d37fb106a4416eba344cc0c5109a4ca
foooomio <foooomio@gmail.com>
2018-01-20 15:57:26 +0900

    Add update_checker.user.js

---
+++ tools/update_checker.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Nicorepo ja-jp.js Update Checker
+// @namespace    https://foooomio.net/
+// @version      0.1
+// @author       foooomio
+// @license      MIT License
+// @match        http://www.nicovideo.jp/my/top
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    const [match, version, ...rest] = /ja-jp.js\?(\d{8})/.exec(document.body.innerHTML);
+
+    if (version !== '20171212') {
+        alert(`ja-jp.js が更新されています。( ${match} )`);
+    }
+})();

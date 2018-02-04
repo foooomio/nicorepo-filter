5a45768ff98b6d7ad30ec8698bc791a895d10f21
foooomio <foooomio@gmail.com>
2018-02-05 01:56:47 +0900

    Remove onInstalled listener

--- src/js/background.js
+++ src/js/background.js
@@ -4,10 +4,8 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        if (message === 'show_page_action') {
-            chrome.pageAction.show(sender.tab.id);
-        }
-    });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === 'show_page_action') {
+        chrome.pageAction.show(sender.tab.id);
+    }
 });

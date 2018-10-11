7a00df3495adaa59f56df8a94b1787ff3c7c04c2
foooomio <foooomio@gmail.com>
2018-10-11 21:28:20 +0900

    Improve build script

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+NAME="nicorepo-filter"
+VERSION=$(jq -r .version src/manifest.json)
 
 mkdir -p dist
 rm dist/*.zip
 
 # for Chrome
-zip -r dist/chrome.zip src -x *.DS_Store
+zip -r "dist/${NAME}-${VERSION}-chrome.zip" src -x "*.DS_Store"
 
 # for Firefox
 cd src
-zip -r ../dist/firefox.zip * -x *.DS_Store
+zip -r "../dist/${NAME}-${VERSION}-firefox.zip" * -x "*.DS_Store"
